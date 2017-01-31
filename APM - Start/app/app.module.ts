b43d709875dcc58aa9d458295249ddc59be791d8
ravi <rshri@gmail.com>
2017-01-31 09:18:07 +0530

    Added routes for navigation

--- APM - Start/app/app.module.ts
+++ APM - Start/app/app.module.ts
@@ -2,21 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { WelcomeComponent }  from './home/welcome.component';
 import { ProductListComponent }  from './products/product-list.component';
 import { ProductFilterPipe } from './products/product-filter.pipe';
 import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail.component';
 
 @NgModule({
   imports: [ BrowserModule,
   	FormsModule,
-  	HttpModule 
+  	HttpModule ,
+  	RouterModule.forRoot([
+  	  {path: 'products' ,component: ProductListComponent},
+  	  {path: 'products/:id' ,component: ProductDetailComponent},
+  	  {path: 'welcome' ,component: WelcomeComponent},
+  	  {path: '' ,redirectTo: 'welcome',pathMatch: 'full'},
+  	  {path: '**' ,redirectTo: 'welcome',pathMatch: 'full'}
+
+  	])
   ],
   declarations: [ AppComponent,
+          WelcomeComponent,
           ProductListComponent,
           ProductFilterPipe,
-          StarComponent
+          StarComponent,
+          ProductDetailComponent
   ],
   bootstrap: [ AppComponent ]
 })
